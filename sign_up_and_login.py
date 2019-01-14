c53d8973487bdb8bce9417de5d0ddfc938d256c4
claudion3 <36760389+claudion3@users.noreply.github.com>
2019-01-14 21:46:49 +0100

    Delete sign_up_and_login.py

--- sign_up_and_login.py
+++
@@ -1,7 +0,0 @@
-def ID():
-    username=input("Create your username:")
-    password1=float=input("create your password")
-    password2=float=input("repute your password")
-
-print("Hello welcome to Sign up")
-print(ID())
