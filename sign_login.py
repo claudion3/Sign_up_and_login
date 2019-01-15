3f375beb80f63de8921d43044d059aff01a1c847
claudion3 <36760389+claudion3@users.noreply.github.com>
2019-01-15 02:05:01 +0100

    add comment

--- sign_login.py
+++ sign_login.py
@@ -1,14 +1,15 @@
-
+#create username and password
 username1=input("Create your usernamer:")
 password1=input("Create your password:")
 password2=input("Repete your password:")
+#verifier if password matched 
 if password1 != password2:
     print("your password is not match please try agian")
     password1=input("Create your password:")
     password2=input("Repete your password:")
 else:
     print("Thank you for sign up")
-
+#choose to login or exit
 print("Press y for login n for exit:")
 massage="y"
 massage=input("")
@@ -17,7 +18,7 @@ if massage=='y':
     password=input("Enter your password:")
     if username !=username1:
         print("Please Create account")
-
+#loop for password        
     while password1 != password:
          password=input("Please try again your password:")
     print("Thank you")
